aa0a1c5da1d9b32a6dfd690e77040ab185c94806
ruanxin <ruanxin@eswin.com>
2021-09-13 10:19:09 +0800

    add gl.py

---
+++ gl.py
@@ -0,0 +1,12 @@
+import EswinAPI
+from ctypes import *
+
+fib=EswinAPI.CEswinUsbDevice()
+print(fib.c())
+print(fib.c1(1,2))
+s=c_ubyte(1)
+n3=c_char_p(1)
+n4=c_uint(1)
+print(fib.Spi_RegRead(n4,byref(s)))
+fab=CDLL(r'C:\Users\e0005202\Desktop\通讯\two\EswinAPI\x64\Release\EswinAPI.dll')
+fab.c()
